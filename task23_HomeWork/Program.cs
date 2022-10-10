92753bf0854eb713db5c4737d2add41453b17ee0
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-10 23:32:38 +0300

    task23_HomeWork/

---
+++ task23_HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 23 Программа, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int getEenteringMean(string mean) // Функция ввода значения и его конвертации из string в int
+{
+    Console.Write($" {mean} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = getEenteringMean("Введите число N: ");
+
+Console.Write(" -> ");
+
+for (int i = 1; i <= number; i++) // инициализация счетечика; проверка условия; увеличение счетчика на 1
+{ 
+    Console.Write($"  {i*i*i}  ");
+}
+
+Console.WriteLine();
