6d4aebafe1b63b1cf275b948ca40130c6f745ad6
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-10 23:31:29 +0300

    task19_HomeWork/Program.cs

--- task19_HomeWork/Program.cs
+++ task19_HomeWork/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Задача 19 Программа, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+*/
+
+Console.Write("Введите пятизначное число: ");
+string userNumber = Console.ReadLine(); // создаем строковую переменную userNumber и присваиваем ей прочтенное из консоли
+int length = userNumber.Length; // обращение к свойству, показывающему количество символов в строке
+
+if (length == 5)
+{
+    if (userNumber[0] == userNumber[4] && userNumber[1] == userNumber[3])
+    {
+        Console.WriteLine($"{userNumber} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{userNumber} -> нет");
+    }
+}
+else
+{
+    Console.WriteLine($"{userNumber} не является пятизначным или целым числом");
+}
